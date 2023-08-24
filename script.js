079114888232d46bfd7b3297da52c02564a10cf2
MelyssaMenezes <melyssa.smenezes@gmail.com>
2023-08-24 16:48:23 -0300

    Criacao do botao enviar e print dos valores dos campos do formulario

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Pagina carregada!");
+
+document.addEventListener('submit',function(event){
+    //evita a pagina ser recarregada ao submeter ao formulario
+    event.preventDefault();
+
+    console.log("Formulario submetido!!");
+
+    // obtem o elemento html com ID fieldName
+    const edtName = document.getElementById('fieldName');
+    // obtem o valor (texto digitado) do edit
+    const nameValue = edtName.value;
+    console.log('Nome:', nameValue);
+
+
+    const edtAddress = document.getElementById('fieldAddress');
+    const addressValue = edtAddress.value;
+    console.log('Endereco:', addressValue);
+    
+
+    const edtAge = document.getElementById('fieldAge');
+    const ageValue = edtAge.value;
+    console.log('Idade:', ageValue);
+});
