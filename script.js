a0eb2991a89f18f2676cf87fcc902b89eb442ddb
MelyssaMenezes <melyssa.smenezes@gmail.com>
2023-08-25 18:45:51 -0300

    Criado a regra do botao salvar para novos registros

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 console.log("Pagina carregada!");
 
+// Lista de clientes (array de objeto)
+let customerList = [];
+
+// Obtem o elemento html com ID fieldName
+const edtName = document.querySelector('#fieldName');
+
+// Obtem o elemento html com ID fieldAddress
+const edtAddress = document.querySelector('#fieldAddress');
+
+// Obtem o elemento html com ID fieldAge
+const edtAge = document.querySelector('#fieldAge');
+
+
+// Funcão disparada quando ocorrer o evento submit
 document.addEventListener('submit', async function(event){
     //evita a pagina ser recarregada ao submeter ao formulario
     event.preventDefault();
 
     console.log("Formulario submetido!!");
-
-    // obtem o elemento html com ID fieldName
-    const edtName = document.getElementById('fieldName');
-    // obtem o valor (texto digitado) do edit
-    const nameValue = edtName.value;
-    console.log('Nome:', nameValue);
-
-
-    const edtAddress = document.getElementById('fieldAddress');
-    const addressValue = edtAddress.value;
-    console.log('Endereco:', addressValue);
-
-
-    const edtAge = document.getElementById('fieldAge');
-    const ageValue = edtAge.value;
-    console.log('Idade:', ageValue);
+    saveCustomer();
 });
+
+function saveCustomer(){
+    console.log('Função saveCustomer executado!');
+
+    // Obtem os valores (texto digitado) dos edits
+    const edtNameValue = edtName.value;
+    const edtAddressValue = edtAddress.value;
+    const edtAgeValue = edtAge.value;
+
+    console.log(edtNameValue, edtAddressValue, edtAgeValue);
+
+    const customer = {
+        name: edtNameValue,
+        address: edtAddressValue,
+        age: edtAgeValue
+    }
+
+    // Adiciona objeto (cliente(customer)) na lista de clientes(array)
+    customerList.push(customer);
+    console.log(customerList);
+}
